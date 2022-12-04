2258ef3c7e6c404b2454ae9216f9a03697b58a4e
aelghaoui <aelghaoui@oppscience.com>
2022-12-04 01:28:50 +0100

    setup the project

---
+++ quickdirtyblog-webapp/src/main/java/com/aelghaoui/quickdirtyblog/StandaloneQuickDirtyBlogApplication.java
@@ -0,0 +1,11 @@
+package com.aelghaoui.quickdirtyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandaloneQuickDirtyBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StandaloneQuickDirtyBlogApplication.class, args);
+    }
+}
